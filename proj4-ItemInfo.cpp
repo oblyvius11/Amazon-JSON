3207e89cdebf52509f1c084dd22c4e8e956edcd1
oblyvius11 <128769225+oblyvius11@users.noreply.github.com>
2023-03-23 22:21:23 -0500

    Update proj4-ItemInfo.cpp

--- proj4-ItemInfo.cpp
+++ proj4-ItemInfo.cpp
@@ -1,15 +1,3 @@
-/**
- * file: proj4-ItemInfo.cpp
- * author: Prof. Aars
- * course: CSI 1440
- * assignment: project 4
- * due date: 2/27/2023
- *
- * date modified:  2/27/2023
- *    - file created
- *
- * This file is the function definitions file.
- */
 
 #include "proj4-ItemInfo.hpp"
 #include <cmath>
